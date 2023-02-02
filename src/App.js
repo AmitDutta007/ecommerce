cd69fd3467d260a8695b075929fcf97aed84fd6f
amitdutta <duttaamit356@gmail.com>
2023-02-02 10:09:59 +0530

    created components and pages also create navbar

--- src/App.js
+++ src/App.js
@@ -2,27 +2,43 @@
 import {
   createBrowserRouter,
   RouterProvider,
+  Outlet
 } from "react-router-dom";
+import Footer from "./components/Footer/Footer";
+import Navbar from './components/Navbar/Navbar';
+import Home from './pages/Home/Home';
+import Product from './pages/Product/Product'
+import Products from './pages/Products/Products'
+
+const Layout = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet/>
+      <Footer />
+    </>
+  )
+}
 
 const router = createBrowserRouter([
   {
     path: "/",
-    // element: <Layout />,
+    element: <Layout />,
     children: [
       {
         path: "/",
-        element: <p>Home page</p>,
-        // <Home />,
+        element:
+          <Home />,
       },
       {
         path: "/products/:id",
-        element: <p>products id page</p>,
-        // <Products />,
+        element:
+          <Products />,
       },
       {
         path: "/product/:id",
-        element: <p>product page</p>,
-        // <Product />,
+        element:
+          <Product />,
       },
     ],
   },
